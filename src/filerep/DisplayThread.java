9a72aff78665d9357c12f5f7bda579c19ea66ad5
wissam-ka <wissam.kabbash@gmail.com>
2014-02-09 15:22:23 +0530

    add threads for archive + display complete

---
+++ src/filerep/DisplayThread.java
@@ -0,0 +1,109 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filerep;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author admin
+ */
+public class DisplayThread implements Runnable
+{
+    Thread t;
+    FileRep fr;
+    FileInputStream fis;
+    Properties pr=new Properties();
+    long oldfilelength;
+    int oldlistlength=0;
+    boolean quit=false;
+            //=new FileInputStream("datafile.dat");
+
+    public DisplayThread(FileRep fr) 
+    {
+        this.fr=fr;    
+        t=new Thread(this);
+        
+    }
+
+    @Override
+    public void run() 
+    {
+        
+        while(!quit)
+        {
+            System.out.println("inside while");
+            File f=new File("datafile.dat");
+            if(f.length()!=oldfilelength)
+            {
+                
+            
+            try 
+            {
+                fis=new FileInputStream("datafile.dat");
+            } 
+            catch (FileNotFoundException ex) 
+            {
+                Logger.getLogger(DisplayThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try 
+            {
+                pr.load(fis);
+                
+                oldfilelength=f.length();
+            }
+            catch (IOException ex) 
+            {
+                Logger.getLogger(DisplayThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                try {
+                    fis.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(DisplayThread.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            DispOnta();
+            if(pr.size()>0)
+            {
+                if(((String)pr.get(String.valueOf(pr.size()-1))).equals("Done"))
+                {
+                quit=true;
+                }
+            }
+                            
+        }
+    }
+    public void DispOnta()
+    {
+        
+        if(pr.isEmpty())
+            {
+                fr.ta.append("\n No files found");
+            }
+            else
+            {
+            for(int j=oldlistlength ;j<pr.size();j++)
+            {
+                
+                   
+                    fr.ta.append("*"+j+"--");
+                   
+                   fr.ta.append(pr.get(String.valueOf(j))+"\n");
+               
+            }
+           
+            oldlistlength=pr.size();
+            
+            }
+    }
+    
+    
+}
---
+++ src/filerep/MultiThread.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filerep;
+
+import java.awt.*;
+import java.nio.file.Path;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+
+/**
+ *
+ * @author admin
+ */
+public class MultiThread  implements Runnable
+{
+
+    
+    Thread t;
+    String search_type,string_msg;
+    Path p1,p2;
+    int[] select_c;
+    boolean arch_ty;
+    List<String> lor=new ArrayList<String>();
+    
+    MultiThread(String search_type, Path p1,Path p2,int[] bsel)
+    {
+        this.search_type=search_type;
+        this.p1=p1;
+        this.p2=p2;
+        select_c=bsel;
+        t=new Thread(this);
+     }
+
+    public MultiThread(String search_type,String string_msg,boolean arch_ty) 
+    {
+        this.search_type=search_type;
+        this.arch_ty=arch_ty;
+        this.string_msg=string_msg;
+         t=new Thread(this);
+    }
+   
+
+    @Override
+    public void run() 
+    {
+        System.out.println("wwwwwwwwwwwwwwwwww");
+        if(search_type.equals("arch"))
+        {
+          FindRepFile f=new FindRepFile(string_msg,arch_ty);
+          f.doSearch();    
+         }
+        else if(search_type.equals("normal"))
+        {
+            FileSearch fs=new FileSearch (p1,p2,select_c);
+            fs.doSearch();
+            lor=fs.searchResults();        
+        }
+    }
+    
+    
+}
